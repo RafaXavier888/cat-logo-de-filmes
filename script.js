c8a06fafbeebf28acf007993b6abec9affdb7959
Rafael Henrique Xavier Trombin <134017823+RafaXavier888@users.noreply.github.com>
2024-02-06 19:36:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,3 @@ function showGenre(genre) {
     // Implemente a lógica para exibir os filmes do gênero selecionado
     console.log("Filmes do gênero " + genre);
   }
-
-function toggleSynopsis(element) {
-    const synopsis = element.previousSibling;
-    synopsis.classList.toggle('show-more');
-    element.textContent = synopsis.classList.contains('show-more') ? 'Ler menos' : 'Ler mais';
-  }
-  
